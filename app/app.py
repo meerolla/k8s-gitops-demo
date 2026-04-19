36d3943cbea3bc05e4088bbb2c3b550723cf0ef3
Mallesh Meerolla <meerolla.mallesh@gmail.com>
2026-04-19 13:50:26 -0500

    add new code

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from GitOps Kubernetes!"
+    return "Hello from GitOps Kubernetes and many more!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
